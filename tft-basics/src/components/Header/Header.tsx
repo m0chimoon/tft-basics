26f080137b867b5c2e3c86abf0d5905dc16af9cc
m0chi/ <aly0mal99@gmail.com>
2024-06-05 19:59:17 +0100

    components templates

---
+++ tft-basics/src/components/Header/Header.tsx
@@ -0,0 +1,26 @@
+const Header = () => {
+ return (
+  <header>
+   <img/>
+   <ul>
+    <li>
+     introduction
+    </li>
+    <li>
+     set
+    </li>
+    <li>
+     comps
+    </li>
+    <li>
+     augments
+    </li>
+    <li>
+     items
+    </li>
+   </ul>
+  </header>
+ )
+}
+
+export default Header
