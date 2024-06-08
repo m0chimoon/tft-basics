622e45795e0832d3fc726745908a6ba22769091d
m0chi/ <aly0mal99@gmail.com>
2024-06-08 20:20:44 +0100

    routing

--- tft-basics/src/App.tsx
+++ tft-basics/src/App.tsx
@@ -1,14 +1,28 @@
-import './App.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Header from './components/Header/Header'
 import Footer from './components/Footer/Footer'
+import Main from './components/Main/Main'
+import Introduction from './components/Introduction/Introduction'
+import Set from './components/Set/Set'
+import Comps from './components/Comps/Comps'
+import Augments from './components/Augments/Augments'
+import Items from './components/Items/Items'
 
 function App() {
 
   return (
-    <>
-    <Header />
-    <Footer />
-    </>
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path='/' element={<Main />} />
+        <Route path='/introduction' element={<Introduction />} />
+        <Route path='/set' element={<Set />} />
+        <Route path='/comps' element={<Comps />} />
+        <Route path='/augments' element={<Augments />} />
+        <Route path='/items' element={<Items />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
   )
 }
 
