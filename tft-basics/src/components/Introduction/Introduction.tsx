7394d852fc3101f423ae4aa02c161e98dc109a70
m0chi/ <aly0mal99@gmail.com>
2024-06-10 13:49:57 +0100

    template of content

--- tft-basics/src/components/Introduction/Introduction.tsx
+++ tft-basics/src/components/Introduction/Introduction.tsx
@@ -1,7 +1,22 @@
 const Introduction = () => {
  return (
   <div>
-    introduction
+    <h2>What is Teamfight Tactics?</h2>
+    <p>
+      Teamfight Tactics is a strategic free-for-all game of attrition featuring iconic champions from League of Legends.
+      Your goal? Grow your team and protect your board to become the last player standing. Here you'll find the basic information
+      you need to get started:
+    </p>
+    <h3>Gameplay</h3>
+    <p>
+    Teamfight Tactics is an autobattler: a round-based PvP game where you’ll draft a team that battles automatically on your behalf.
+    If you've never played an autobattler before, don't worry! TFT borrows from many of the concepts you know and love about other strategy games,
+    like drafting from a shared pool of resources, team building, economy management, and positioning.
+
+    Here's how it works: Individuals face off in 1-on-1 matches until only one player remains. You win by drafting the best team of champions from a shared pool,
+    equipping them with powerful items, managing your gold income to buy out the competition, and positioning your team in winning battle formations.
+    There are unlimited winning strategies, so you’ll have to adapt to different situations in every game to come out on top.
+    </p>
   </div>
  )
 }
--- tft-basics/src/components/Set/Set.tsx
+++ tft-basics/src/components/Set/Set.tsx
@@ -1,7 +1,12 @@
 const Set = () => {
  return (
   <div>
-    set
+    <h2>Sets</h2>
+    <p>
+      Every game of TFT takes place in the Convergence, and centers around a specific theme and story called a set.
+      Every few months TFT introduces you to a new universe with new champions, traits, and characters to play.
+      In terms of gameplay, new sets also come with a new Ranked ladder to climb, new team compositions, and new item possibilities.
+    </p>
   </div>
  )
 }
