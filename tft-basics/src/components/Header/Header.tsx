410794f84a3f6ce8dbc70d8db61a0c94ca578914
m0chi/ <aly0mal99@gmail.com>
2024-06-10 13:27:00 +0100

    header styling

--- tft-basics/src/components/Header/Header.tsx
+++ tft-basics/src/components/Header/Header.tsx
@@ -1,29 +1,31 @@
 import { Link } from 'react-router-dom'
 import tftlogo from '../../assets/tftbasics.png'
+import tfttextlogo from '../../assets/tftbasicstextlogo.png'
 
 const Header = () => {
   return (
-    <header className='flex w-[1280px] items-center py-1 uppercase'>
-    <img className='max-w-[96px] max-h-[96px]' src={tftlogo}/>
-    <ul className='flex gap-10 px-6'>
-      <li>
-        <Link to='/introduction'>introduction</Link>
-      </li>
-      <li>
-        <Link to='/set'>set</Link>
-      </li>
-      <li>
-        <Link to='/comps'>comps</Link>
-      </li>
-      <li>
-        <Link to='/augments'>augments</Link>
-      </li>
-      <li>
-        <Link to='/items'>items</Link>
-      </li>
-   </ul>
-  </header>
- )
+    <header className='flex w-[1280px] items-center py-1 uppercase text-lg font-semibold text-[#ebc777]'>
+      <img className='max-w-[96px] max-h-[96px] mr-1.5' src={tfttextlogo}/>
+      <img className='max-w-[96px] max-h-[96px]' src={tftlogo}/>
+      <ul className='flex gap-10 px-6'>
+        <li className='hover:underline'>
+          <Link to='/introduction'>introduction</Link>
+        </li>
+        <li className='hover:underline'>
+         <Link to='/set'>set</Link>
+        </li>
+        <li className='hover:underline'>
+          <Link to='/comps'>comps</Link>
+        </li>
+        <li className='hover:underline'>
+          <Link to='/augments'>augments</Link>
+        </li>
+        <li className='hover:underline'>
+          <Link to='/items'>items</Link>
+        </li>
+      </ul>
+    </header>
+  )
 }
 
 export default Header
