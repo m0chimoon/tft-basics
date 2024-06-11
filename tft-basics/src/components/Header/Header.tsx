dc33df16b1a4e27ce9494937a69e3305a0f4b162
m0chi/ <aly0mal99@gmail.com>
2024-06-11 14:01:35 +0100

    fix image + routing to main page

--- tft-basics/src/components/Header/Header.tsx
+++ tft-basics/src/components/Header/Header.tsx
@@ -5,7 +5,7 @@ import tfttextlogo from '../../assets/tftbasicstextlogo.png'
 const Header = () => {
   return (
     <header className='flex w-[1280px] items-center mb-12 py-1 uppercase text-lg font-semibold text-[#ebc777]'>
-       <Link to='/main'>
+       <Link to='/'>
         <img className='max-w-[96px] max-h-[96px] mr-1.5' src={tfttextlogo}/>
        </Link>
        
