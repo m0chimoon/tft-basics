57565855f38c532c7dad48423b79664cdc7ab00e
m0chi/ <aly0mal99@gmail.com>
2024-06-11 13:49:38 +0100

    css

--- tft-basics/src/components/Set/Set.tsx
+++ tft-basics/src/components/Set/Set.tsx
@@ -1,8 +1,8 @@
 const Set = () => {
  return (
   <div className='flex flex-col m-auto max-w-2xl'>
-    <h2>Sets</h2>
-    <p>
+    <h2 className="text-4xl font-bold uppercase mb-5">Sets</h2>
+    <p className="text-lg text-justify">
       Every game of TFT takes place in the Convergence, and centers around a specific theme and story called a set.
       Every few months TFT introduces you to a new universe with new champions, traits, and characters to play.
       In terms of gameplay, new sets also come with a new Ranked ladder to climb, new team compositions, and new item possibilities.
