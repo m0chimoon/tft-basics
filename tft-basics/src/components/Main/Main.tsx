5fc4872aa5a26ae5ea14c6425d2fa4f68ec1245d
m0chi/ <aly0mal99@gmail.com>
2024-06-14 22:36:41 +0100

    main image

--- tft-basics/src/components/Main/Main.tsx
+++ tft-basics/src/components/Main/Main.tsx
@@ -1,7 +1,9 @@
+import tftlanding from "../../assets/tftlanding.png"
+
 const Main = () => {
   return (
-    <main>
-
+    <main className="flex max-w-[1280px]">
+      <img src={tftlanding} className="rounded-lg drop-shadow-lg" />
     </main>
  )
 }
