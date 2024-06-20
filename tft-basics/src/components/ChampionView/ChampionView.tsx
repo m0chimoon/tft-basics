f3cc43dd2b73c1845a276e0f7bf968637efe91ac
Simon <frasselk@gmail.com>
2024-06-20 16:46:01 +0200

    styiling changes

--- tft-basics/src/components/ChampionView/ChampionView.tsx
+++ tft-basics/src/components/ChampionView/ChampionView.tsx
@@ -12,6 +12,7 @@ const ChampionView = () => {
 
             <div className='flex flex-col place-items-center gap-y-4 w-4/12'>
                 <img className={`border ${championColor}`} src={`/src/assets/champions/${name}.png`} />
+                <h2 className='text-4xl font-bold mb-5 text-[#f6b03f]'>{name}</h2>
                 <div className="bg-gray-600 p-4 rounded shadow-lg">
                     <h3 className="text-2xl font-bold text-[#f6b03f]">Ability</h3>
                     <p className="text-xl">{champion.ability.name}</p>
@@ -20,11 +21,10 @@ const ChampionView = () => {
             </div>
             
             <div className='flex flex-col justify-center gap-x-2 w-8/12'>
-                <h2 className='text-4xl font-bold mb-5 text-[#f6b03f]'>{name}</h2>
-                <div className="bg-gray-600 p-4 rounded shadow-lg">
+                <div className="bg-gray-700 p-4 rounded shadow-lg">
                     <p className="text-xl">Cost: {champion.cost}</p>
                     <hr className='m-2 bg-gray-400 h-0.5 border-none'/>
-                    <p className="text-xl">Traits</p>
+                    <p className="text-2xl">Traits</p>
                     {champion.traits.map(trait => (
                         <Traits key={trait} trait={trait}></Traits>
                     ))}
