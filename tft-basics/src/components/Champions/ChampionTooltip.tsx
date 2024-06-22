8990a4979845e38899f6df666f72e975ab4faab4
Simon <frasselk@gmail.com>
2024-06-22 18:04:27 +0200

    dynamically appears above or below

--- tft-basics/src/components/Champions/ChampionTooltip.tsx
+++ tft-basics/src/components/Champions/ChampionTooltip.tsx
@@ -1,8 +1,8 @@
-import { useState } from "react";
+import { useState, useRef, useEffect } from "react";
 import { setChampColor, Cost, Champion } from "./Champions";
 
 interface IToolTipProps {
-    children: string | JSX.Element | JSX.Element[] 
+    children: string | JSX.Element | JSX.Element[]
     champion: Champion
 }
 
@@ -10,6 +10,9 @@ const ChampionTooltip = (props: IToolTipProps) => {
     let timeout: any;
     const champion = props.champion;
     const [active, setActive] = useState(false);
+    const [position, setPosition] = useState('bottom');
+    const triggerRef = useRef<HTMLDivElement>(null);
+    const tooltipRef = useRef<HTMLDivElement>(null);
 
     const showTip = () => {
         timeout = setTimeout(() => {
@@ -22,19 +25,36 @@ const ChampionTooltip = (props: IToolTipProps) => {
         setActive(false);
     };
 
+    useEffect(() => {
+        if (active && triggerRef.current && tooltipRef.current) {
+            const triggerRect = triggerRef.current.getBoundingClientRect();
+            const tooltipRect = tooltipRef.current.getBoundingClientRect();
+            const spaceAbove = triggerRect.top;
+            const spaceBelow = window.innerHeight - triggerRect.bottom;
+
+            if (spaceBelow < tooltipRect.height && spaceAbove >= tooltipRect.height) {
+                setPosition('top');
+            } else {
+                setPosition('bottom');
+            }
+        }
+    }, [active]);
+
     return (
-        <div className="inline-block relative" onMouseEnter={showTip} onMouseLeave={hideTip}>
+        <div className="inline-block relative" onMouseEnter={showTip} onMouseLeave={hideTip} ref={triggerRef}>
             {props.children}
             {active && (
-                <div className={`absolute rounded left-1/2 -translate-x-1/2 text-white bg-indigo-800 text-xl z-10 whitespace-nowrap mt-2 flex flex-col w-32`}>
+                <div
+                    ref={tooltipRef}
+                    className={`absolute rounded left-1/2 -translate-x-1/2 ${position === 'bottom' ? 'top-full mt-2' : 'bottom-full mb-2'} text-white bg-indigo-800 text-xl z-10 whitespace-nowrap flex flex-col w-32`}
+                >
                     <img className={`w-32 h-32 rounded border-2 ${setChampColor(champion.cost)}`} src={`/src/assets/champions/${champion.name}.png`} />
                     <div className="p-2 text-base">
                         {champion.traits.length !== 0 && (
                             <>
-                                {/* <div className="border-l mx-2"/> */}
                                 <div className="">
                                     {champion.traits.map(trait => (
-                                        <div className="flex place-items-center pr-4">
+                                        <div className="flex place-items-center pr-4" key={trait}>
                                             <img className="h-4 w-4 mr-1" src={`/src/assets/traits/${trait}.png`} />
                                             <p>{trait}</p>
                                         </div>
@@ -49,15 +69,14 @@ const ChampionTooltip = (props: IToolTipProps) => {
                             ) : (
                                 <>
                                     <p>{champion.cost}</p>
-                                    <img className="" src="/src/assets/coin.svg" />
+                                    <img src="/src/assets/coin.svg" />
                                 </>
                             )}
                         </div>
                     </div>
-
                 </div>
             )}
         </div>
     )
 }
-export default ChampionTooltip
+export default ChampionTooltip;
