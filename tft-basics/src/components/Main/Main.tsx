56805f8e00303c665dd052816d7dada6715d7935
m0chi/ <aly0mal99@gmail.com>
2024-07-15 09:11:14 +0100

    landing page css fix

--- tft-basics/src/components/Main/Main.tsx
+++ tft-basics/src/components/Main/Main.tsx
@@ -5,9 +5,9 @@ import tfthorn from "../../assets/tfthorn.png"
 const Main = () => {
   return (
     <main className="flex justify-center mt-14">
-      <img src={tftpengu} className="w-1/12 animate-wiggle animate-infinite animate-duration-[3400ms] animate-ease-in-out" />
-      <img src={tftlanding} className="w-6/12 mx-12 max-md:mx-5"/>
-      <img src={tfthorn} className="w-[11%] animate-wiggle animate-infinite animate-duration-[3400ms] animate-ease-in-out animate-reverse" />
+      <img src={tftpengu} className="w-[160px] animate-wiggle animate-infinite animate-duration-[3400ms] animate-ease-in-out" />
+      <img src={tftlanding} className="w-6/12 h-2/4 mx-12 max-md:mx-5"/>
+      <img src={tfthorn} className="w-[180px] h-[200px] animate-wiggle animate-infinite animate-duration-[3400ms] animate-ease-in-out animate-reverse" />
     </main>
  )
 }
