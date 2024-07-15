677457ac28a5950f4680d6c9f0e1ebfce4bb33a9
m0chi/ <aly0mal99@gmail.com>
2024-07-15 09:18:53 +0100

    css fix

--- tft-basics/src/components/Main/Main.tsx
+++ tft-basics/src/components/Main/Main.tsx
@@ -4,10 +4,10 @@ import tfthorn from "../../assets/tfthorn.png"
 
 const Main = () => {
   return (
-    <main className="flex justify-center mt-14">
-      <img src={tftpengu} className="w-[160px] animate-wiggle animate-infinite animate-duration-[3400ms] animate-ease-in-out" />
-      <img src={tftlanding} className="w-6/12 h-2/4 mx-12 max-md:mx-5"/>
-      <img src={tfthorn} className="w-[180px] h-[200px] animate-wiggle animate-infinite animate-duration-[3400ms] animate-ease-in-out animate-reverse" />
+    <main className="flex justify-center mt-[10%]">
+      <img src={tftpengu} className="w-[10%] animate-wiggle animate-infinite animate-duration-[3400ms] animate-ease-in-out" />
+      <img src={tftlanding} className="w-6/12 h-2/4 mx-12 "/>
+      <img src={tfthorn} className="w-[10%] h-[14%] animate-wiggle animate-infinite animate-duration-[3400ms] animate-ease-in-out animate-reverse" />
     </main>
  )
 }
